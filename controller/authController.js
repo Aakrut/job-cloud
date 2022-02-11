fb02deb4f1757abef139ddcec8dd4b7f6a2392aa
aakrut dabhi <aakrutdabhi@gmail.com>
2022-02-11 19:49:21 +0530

    backend

--- controller/authController.js
+++ controller/authController.js
@@ -1,13 +1,37 @@
-const register =  (req, res) => {
-    res.end('Register');
-}
+import { StatusCodes } from "http-status-codes";
+import User from "../models/User.js";
 
-const login =  (req, res) => {
-    res.end('Login')
-}
+import {BadRequestError} from '../error/index.js'
 
-const updateUser =  (req, res) => {
-    res.send('Update User');
-}
+const register = async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-export {register,login,updateUser}
+  if (!name || !email || !password) {
+    throw new BadRequestError("please provide all values");
+  }
+
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use')
+  }
+  const user = await User.create({ name, password, email });
+  res.status(StatusCodes.CREATED).json({ user });
+};
+
+const login = async (req, res) => {
+  try {
+    res.end("Login");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    res.send("Update User");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { register, login, updateUser };
