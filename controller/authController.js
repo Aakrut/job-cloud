fcb25e920f02664f91fe37acda116f01dd56d604
aakrut dabhi <aakrutdabhi@gmail.com>
2022-02-12 13:54:47 +0530

    authentication

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/User.js";
 
-import {BadRequestError} from '../error/index.js'
+import { BadRequestError, UnAuthenticatedError } from "../error/index.js";
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -12,18 +12,43 @@ const register = async (req, res, next) => {
 
   const userAlreadyExists = await User.findOne({ email });
   if (userAlreadyExists) {
-    throw new BadRequestError('Email already in use')
+    throw new BadRequestError("Email already in use");
   }
   const user = await User.create({ name, password, email });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+  });
 };
 
 const login = async (req, res) => {
-  try {
-    res.end("Login");
-  } catch (error) {
-    console.log(error);
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please Provide All Values");
   }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    throw new UnAuthenticatedError("Invalid Credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+
+  user.password = undefined;
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 
 const updateUser = async (req, res) => {
--- server.js
+++ server.js
@@ -6,12 +6,16 @@ import dotenv from 'dotenv';
 import authRouter from './routes/authRoutes.js'
 import jobsRouter from './routes/jobsRoutes.js'
 import 'express-async-errors'
+import morgan from 'morgan'
 
 
 dotenv.config()
 
 import connectDB from './db/connect.js'
 
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'))
+}
 app.use(express.json())
 
 
@@ -22,8 +26,6 @@ app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000;
-
-
 
 
 const start = async () => {
